a9eeb6ccbae8e9770247c2161f1b0bbcc97f53fb
Coder246 <tim-arnold@outlook.de>
2020-08-13 21:25:52 +0200

    Updated Statistics to the new Plugin System

--- src/de/ft/statistics/Main.java
+++ src/de/ft/statistics/Main.java
@@ -1,49 +1,35 @@
 package de.ft.statistics;
 
-import com.badlogic.gdx.Input;
 import com.kotcrab.vis.ui.widget.Menu;
 import com.kotcrab.vis.ui.widget.MenuItem;
-import com.kotcrab.vis.ui.widget.VisLabel;
-import com.kotcrab.vis.ui.widget.VisTable;
-import de.ft.interitus.Block.BlockVar;
-import de.ft.interitus.ProgrammingSpace;
-import de.ft.interitus.UI.MenuBar;
-import de.ft.interitus.UI.shortcut.ShortCut;
-import de.ft.interitus.UI.shortcut.SpecialKeys;
-import de.ft.interitus.data.user.experience.ExperienceVar;
-import de.ft.interitus.plugin.Configuration;
-import de.ft.interitus.plugin.Plugin;
-import de.ft.interitus.plugin.PluginManagerHandler;
-import de.ft.interitus.plugin.PluginRegister;
 
+import de.ft.interitus.Programm;
+import de.ft.interitus.Var;
+import de.ft.interitus.datamanager.programmdata.experience.ExperienceVar;
+import de.ft.interitus.plugin.*;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 
 public class Main implements Plugin {
-PluginRegister pluginRegister =new PluginRegister();
-    MenuItem blockanzahl=new MenuItem("Blöcke: ");
-    MenuItem wireanzahl=new MenuItem("Wires: ");
-    MenuItem stunden=new MenuItem("stunden: ");
+
+    MenuItem blockanzahl = new MenuItem("Blöcke: ");
+    MenuItem wireanzahl = new MenuItem("Wires: ");
+    MenuItem stunden = new MenuItem("stunden: ");
 
     @Override
     public boolean register() {
 
 
-        pluginRegister.config(Configuration.name,"statistics");
-        pluginRegister.config(Configuration.version,"9.0");
-        PluginManagerHandler.register(pluginRegister);
-        Menu menue=new Menu("Statistics");
+
+        Menu menue = new Menu("Statistics");
         menue.addItem(new MenuItem("open Statistics"));
-       blockanzahl.setDisabled(true);
-       wireanzahl.setDisabled(true);
-       stunden.setDisabled(true);
-       menue.addItem(wireanzahl);
-       menue.addItem(blockanzahl);
-       menue.addItem(stunden);
-       PluginManagerHandler.addMenuEntry(menue);
+        blockanzahl.setDisabled(true);
+        wireanzahl.setDisabled(true);
+        stunden.setDisabled(true);
+        menue.addItem(wireanzahl);
+        menue.addItem(blockanzahl);
+        menue.addItem(stunden);
+        PluginGateway.addMenuEntry(menue,this);
 
 
         return true;
@@ -59,11 +45,45 @@ PluginRegister pluginRegister =new PluginRegister();
     public boolean run() {
 
 
-                blockanzahl.setText("Blöcke: "+ BlockVar.blocks.size());
-                wireanzahl.setText("Wires: "+BlockVar.wires.size());
-                stunden.setText("stunden: "+ Math.round(ExperienceVar.programmtimeinhoures));
+        try {
+            try {
+                blockanzahl.setText("Blöcke: " + Var.openprojects.get(Var.openprojectindex).blocks.size());
+                wireanzahl.setText("Wires: " + Var.openprojects.get(Var.openprojectindex).wires.size());
+            }catch (IndexOutOfBoundsException e) {
+                blockanzahl.setText("Blöcke: " + "0");
+                wireanzahl.setText("Wires: " + "0");
 
-return true;
+            }
+            stunden.setText("stunden: " + Math.round(ExperienceVar.programmtimeinhoures));
+        } catch (NoSuchFieldError e) {
+
+        }
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "Statistics";
+    }
+
+    @Override
+    public double getVersion() {
+        return 1.0;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Dieses Plugin zeigt dir Statistiken an";
+    }
+
+    @Override
+    public String getLongDescription() {
+        return "Es werden die benutzen Blöcke, die Wires und die verbrachten Stunden im Programm angezeigt";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "FT-Interitus";
     }
 
 }
